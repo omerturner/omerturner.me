c3dfca8230ca293a8dcb447097f8c6c9be8d55f0
omerturner <omerturner@gmail.com>
2016-01-05 21:03:21 +0200

    resized imgs

--- server.js
+++ server.js
@@ -9,13 +9,17 @@ mongoose.connect('mongodb://localhost:27017/portfolio');
 app.use(express.static(__dirname + "/src"));
 app.use(bodyParser.json());
 
-//REST API
-app.get('/api/works', worksController.list);
-
 app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.status(404).send('Page Not Found');
   res.status(500).send('Something is broken');
+});
+
+//REST API
+app.get('/api/works', worksController.list);
+
+app.get('/*', function(req, res){
+    res.sendFile(__dirname + '/src/index.html');
 });
 
 app.listen(3000);
